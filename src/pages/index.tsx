953c3e6ea3ee2d1aa2a5e7269824af207e2266cb
Kerstin Huppenbauer <k.huppenbauer@web.de>
2022-10-13 20:43:45 +0200

    TASK: Add initial layout to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Text } from '@mantine/core';
 import Layout from '../components/Layout/Layout';
+import PageFooter from '../components/Page/Footer';
+import PageHeader from '../components/Page/Header';
+import Container from '../components/Container/Container';
 
 export default function HomePage() {
   return (
     <Layout>
-      <Text>Hello World</Text>
+      <PageHeader />
+      <main>
+        <Container />
+      </main>
+      <PageFooter />
     </Layout>
   );
 }
